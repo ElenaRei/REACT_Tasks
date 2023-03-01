25259ad7380a453fea83b6910a7263754fe07a91
ElenaRei <113910361+ElenaRei@users.noreply.github.com>
2023-03-01 22:05:01 +0100

    Lesson 010323

---
+++ 2/myapp/src/components/Task.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Task({ title, completed }) {
+
+  const status = completed ? 'done' : 'not done';
+
+  return (
+    <div>
+      <p>Title: { title }</p>
+      <p>Status: { status }</p>
+    </div>
+  )
+}
